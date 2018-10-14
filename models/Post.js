22eb62c1ddb0148042957e2059f3c39538a911f1
yuryol <yury0l@rambler.ru>
2018-10-14 20:44:45 +0300

    ап
    ап

---
+++ models/Post.js
@@ -0,0 +1,20 @@
+//создание модели
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+	title: {
+		type: String,
+		require: true
+	},
+	text: {
+		type: String,
+		require: true
+	},	
+	date: {
+		type: Date,
+		default: Date.now
+	}
+})
+
+module.exports = mongoose.model('post', postSchema) //экспорт наружу данной модели. хотим зарегистрировать новую коллекцию пот и модель
